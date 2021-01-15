afdca4d47c53a0d5a39c8b29cc6dc38547377579
Jun Jinhee <wjswlsgml93@gmail.com>
2021-01-16 00:56:28 +0900

    20200116 백준 1단계 10718

---
+++ src/com/jinhee/baekjoon/step01/practice10718.java
@@ -0,0 +1,9 @@
+package com.jinhee.baekjoon.step01;
+
+public class practice10718 {
+	public static void main(String[] args) {
+		System.out.println("강한친구 대한육군");
+		System.out.println("강한친구 대한육군");
+		
+	}
+}
