939f2f24ac8ad6477d7d71fb639afd3a3d0cbfa2
Jun Jinhee <wjswlsgml93@gmail.com>
2021-01-16 01:00:52 +0900

    20200116 백준 10171번

---
+++ src/com/jinhee/baekjoon/step01/practice10171.java
@@ -0,0 +1,11 @@
+package com.jinhee.baekjoon.step01;
+
+public class practice10171 {
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+		
+	}
+}
