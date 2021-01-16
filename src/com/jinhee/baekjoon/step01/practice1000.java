ebb23dfec754168403eecd03ce333e742f4c031d
Jun Jinhee <wjswlsgml93@gmail.com>
2021-01-16 22:12:20 +0900

    20200116 백준 1단계 1000번

---
+++ src/com/jinhee/baekjoon/step01/practice1000.java
@@ -0,0 +1,15 @@
+package com.jinhee.baekjoon.step01;
+
+import java.util.Scanner;
+
+public class practice1000 {
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int A = scanner.nextInt();
+		int B = scanner.nextInt();
+		
+		System.out.println(A + B);
+	}
+}
