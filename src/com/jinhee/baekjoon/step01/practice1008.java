b9f260e9708ba8949c07bdf38b1f6ac913e67dd0
Jun Jinhee <wjswlsgml93@gmail.com>
2021-01-16 23:50:23 +0900

    20200116 백준 1단계 1008번

---
+++ src/com/jinhee/baekjoon/step01/practice1008.java
@@ -0,0 +1,14 @@
+package com.jinhee.baekjoon.step01;
+
+import java.util.Scanner;
+
+//20200116 백준 1단계 1008번
+public class practice1008 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double a = sc.nextDouble();
+		double b = sc.nextDouble();
+		
+		System.out.print(a/b);
+	}
+}	
