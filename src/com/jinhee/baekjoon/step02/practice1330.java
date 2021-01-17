38dbda0d0b20bb7f720c822345e05f0c659790e0
Jun Jinhee <wjswlsgml93@gmail.com>
2021-01-17 23:56:54 +0900

    20200117 백준 2단계 수정중

---
+++ src/com/jinhee/baekjoon/step02/practice1330.java
@@ -0,0 +1,15 @@
+package com.jinhee.baekjoon.step02;
+
+import java.util.Scanner;
+
+public class practice1330 {
+	
+	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		
+	}
+}
