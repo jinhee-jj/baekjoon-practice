6e1cb41ae71e79c7e877f3c9061ab3f6407006bf
Jun Jinhee <wjswlsgml93@gmail.com>
2021-01-18 00:05:00 +0900

    20200116 백준 2단계 1330번

--- src/com/jinhee/baekjoon/step02/practice1330.java
+++ src/com/jinhee/baekjoon/step02/practice1330.java
@@ -7,9 +7,17 @@ public class practice1330 {
 	public static void main(String[] args) {
 	
 		Scanner sc = new Scanner(System.in);
-		int A = sc.nextInt();
-		int B = sc.nextInt();
+		int a = sc.nextInt();
+		int b = sc.nextInt();
 		
-		
+		if(-10000 <= a && a <= 10000 && -10000 <= b && b <= 10000) {
+			if(a > b) {
+				System.out.println(">");
+			} else if(a < b) {
+				System.out.println("<");
+			} else {
+				System.out.println("==");
+			}
+		}
 	}
 }
