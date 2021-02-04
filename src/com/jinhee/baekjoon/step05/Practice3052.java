9a608138e25e88ba6a0291096dd068645ca203ad
Jun Jinhee <wjswlsgml93@gmail.com>
2021-02-04 13:36:00 +0900

    백준 5단계 3052번

---
+++ src/com/jinhee/baekjoon/step05/Practice3052.java
@@ -0,0 +1,45 @@
+package com.jinhee.baekjoon.step05;
+
+import java.util.Scanner;
+
+//문제
+//두 자연수 A와 B가 있을 때, A%B는 A를 B로 나눈 나머지 이다. 예를 들어, 7, 14, 27, 38을 3으로 나눈 나머지는 1, 2, 0, 2이다. 
+//
+//수 10개를 입력받은 뒤, 이를 42로 나눈 나머지를 구한다. 그 다음 서로 다른 값이 몇 개 있는지 출력하는 프로그램을 작성하시오.
+//
+//입력
+//첫째 줄부터 열번째 줄 까지 숫자가 한 줄에 하나씩 주어진다. 이 숫자는 1,000보다 작거나 같고, 음이 아닌 정수이다.
+//
+//출력
+//첫째 줄에, 42로 나누었을 때, 서로 다른 나머지가 몇 개 있는지 출력한다.
+public class Practice3052 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] numArr = new int[10];
+		int cnt = 1;
+		
+		for(int i = 0; i < numArr.length; i++) {
+			numArr[i] = sc.nextInt() % 42;
+			
+	//		System.out.print(numArr[i] + " ");
+		}
+		
+		for(int i = 0; i < numArr.length; i++) {
+
+			
+			for(int j = 0; j < i; j++) {
+				 if(numArr[j] == numArr[i]) {
+					 break;
+				 }
+				 
+				 if(j == i-1) {
+					 cnt++;
+				 }
+			}
+			
+		}		
+		System.out.println(cnt);
+
+		
+	}
+}
